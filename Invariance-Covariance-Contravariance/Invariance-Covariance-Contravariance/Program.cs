590d9233efef7c4f136012edbd5df784e1b70cde
Yuriy Parashynets <yura.parashynets@gmail.com>
2025-03-23 16:31:55 +0200

    main - main information

---
+++ Invariance-Covariance-Contravariance/Invariance-Covariance-Contravariance/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Invariance_Covariance_Contravariance;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    // Assignment Compatibility
+    public object GetObject()
+    {
+        string value1 = "value1";
+        object value2 = "value2"; 
+                                  
+        string value3 = value2; // Compiler Error
+
+        return value1;
+    }
+
+    // Conversion for Arrays and Collections
+    public object[] GetArray()
+    {
+        object[] array = new string[3];
+        array[0] = "";
+        array[1] = 5; // RunTime Error
+
+        return array;
+    }
+
+    // Conversion for Generic Interfaces
+    interface IExample<T> { }
+
+    //Invariance approach
+    IExample<int> example = (IExample<int>)null;
+
+    IExample<string> example2 = (IExample<string>)null; 
+
+    //Covariance approach
+    IExample<object> example3 = (IExample<string>)null; // Compiler Error
+
+    //Contravariance approach
+    IExample<string> example4 = (IExample<object>)null; // Compiler Error
+}
